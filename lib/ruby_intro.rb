d5b32923357f224b4319be6712f5a1b2f4f7c72d
Logan Zeien <lzeien@uccs.edu>
2023-02-15 19:08:43 +0000

    Finished part 4

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -35,11 +35,27 @@ def starts_with_consonant? s
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s.downcase.match(/^[0-1]*$/) && s.downcase.match(/[00]$/) #dont combine these regex, it doesnt work
+    return true
+  end
+  return false
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+
+  def initialize(_isbn, _price)
+    if _isbn.empty? || _price <= 0
+      raise ArgumentError.new
+    else
+      @isbn = _isbn
+      @price = _price
+    end
+  end
+
+  def price_as_string()
+    return "$" + sprintf('%.2f', @price) # This nifty sprintf came from https://www.bing.com/ck/a?!&&p=5cbad89e5aa45c83JmltdHM9MTY3NjQxOTIwMCZpZ3VpZD0zNzA3N2Q3NC00YjA5LTZjODctMTM5NC02ZmQ0NGFlZjZkNDAmaW5zaWQ9NTQyMQ&ptn=3&hsh=3&fclid=37077d74-4b09-6c87-1394-6fd44aef6d40&psq=ruby+round+a+float+to+two+decimal+places&u=a1aHR0cHM6Ly9zdGFja292ZXJmbG93LmNvbS9xdWVzdGlvbnMvMTA0NTk5MDEvaG93LWRvLXlvdS1yb3VuZC1hLWZsb2F0LXRvLTItZGVjaW1hbC1wbGFjZXMtaW4tanJ1Ynk&ntb=1 by Theo
+  end
 end
